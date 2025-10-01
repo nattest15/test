31b53dc5fb54916e7f063dfb07c3fe0f9ff11f85
Natalia <nattest15@gmail.com>
2025-10-01 12:52:39 +0200

    fix: fixing the fixtures

--- e2e/fixtures/loginPage.fixture.ts
+++ e2e/fixtures/loginPage.fixture.ts
@@ -2,17 +2,18 @@ import { LoginPage } from "../pages/loginPage";
 import { test as baseTest } from "@playwright/test";
 import { authenticator } from "otplib";
 interface IPages {
-  loginPage: LoginPage
+  loginPage: LoginPage;
 }
+
 export const loginPageTest = baseTest.extend<IPages>({
   loginPage: [
     async ({ page }, use) => {
       const loginPage = new LoginPage(page);
       await use(loginPage);
     },
-    { auto: true }],
+    { auto: true },
+  ],
 });
-
 export const generateTotopTest = baseTest.extend<{ generateTotp: string }>({
   generateTotp: async ({}, use) => {
     const secret = process.env.TOTP_SECRET;
